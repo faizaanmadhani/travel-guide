ee424c311d14339e1397ea44342cd907dd40cbc8
Faizaan Madhani <fzmadhani@gmail.com>
2022-05-26 11:26:02 -0400

    undockerize node and add nodemon

--- server/src/provider.ts
+++ server/src/provider.ts
@@ -1,11 +1,36 @@
 import { DataSource } from "apollo-datasource";
 import { QueryBookArgs } from "./generated/graphql";
-import { connect } from "mongoose";
+import { Schema, model, connect } from "mongoose";
+
+interface IUser {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  avatar: String,
+});
+
+const User = model<IUser>("User", userSchema);
 
 const run = async () => {
-  const url = "mongodb://mongo:27017/my_db";
+  const url = "mongodb://localhost:27017/my_db";
   await connect(url);
+
+  const user = new User({
+    name: "Bill",
+    email: "bill@initech.com",
+    avatar: "https://i.imgur.com/dM7Thhn.png",
+  });
+  await user.save();
+
+  console.log(user.email); // 'bill@initech.com'
 };
+
+run().catch((error) => console.log(error));
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
