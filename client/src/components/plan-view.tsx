dd5bdfaddd9ee8dc948317475d26544dca8ca6e1
Jiawen Zhu <s234zhu@uwaterloo.ca>
2022-06-02 18:56:42 -0400

    travel plan card view ui v0
    skeleton

--- client/src/components/plan-view.tsx
+++ client/src/components/plan-view.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { NativeBaseProvider, Text, Box, extendTheme, Center } from "native-base";
+import { NativeBaseProvider, Text, Box, extendTheme, Center, Image, HStack, VStack } from "native-base";
+import { Entypo } from '@expo/vector-icons';
 import { UserProvider } from "../../../server/src/provider"
 
 // "condensed" view of travel plan, used on home page etc.
@@ -11,7 +12,25 @@ export default function PlanView() {
             <Center>
                 <Box bg="tertiary.300" width={'90%'}>
                     <Center>
-                        I am a travel plan
+                        <HStack>
+                            <Image source={{
+                                uri: "https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/heroImage1/PowerfulReasons_hero.jpg"
+                                }} alt="Alternate Text" size="xl" />
+                            <VStack>
+                                <Box bg="secondary.200">
+                                    Tag 1
+                                </Box>
+                                <Box bg="secondary.200">
+                                    Tag 2
+                                </Box>
+                                <Box bg="secondary.200">
+                                    Tag 3
+                                </Box>
+                                <Entypo name="dots-three-horizontal" size={24} color="black" />
+                            </VStack>
+                        </HStack>
+                       
+                            Name of Travel Plan
                     </Center>
                 </Box>     
             </Center>
