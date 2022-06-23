1d45bc42cc808caa19ab63e4a63eb41908f0e00e
Faizaan Madhani <fzmadhani@gmail.com>
2022-06-23 11:40:21 -0400

    Revert "Travel Plan Card View v1 (#21)"
    This reverts commit 2f9dd82dde02fc579b6dbce74106dd9cfb9c994f.

--- client/src/components/plan-view.tsx
+++ client/src/components/plan-view.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { NativeBaseProvider, Button, SunIcon, Text, Box, Icon, extendTheme, Center, Image, HStack, VStack, Container, AspectRatio, Divider } from "native-base";
+import { NativeBaseProvider, Text, Box, extendTheme, Center, Image, HStack, VStack, Container, AspectRatio, Divider } from "native-base";
 import { Entypo } from '@expo/vector-icons';
 import { UserProvider } from "../../../server/src/provider"
 import { gql, useQuery, ApolloProvider } from "@apollo/client";
@@ -22,7 +22,7 @@ const GET_PLANS = gql`query GetPlan {
     }
   }`;
 
-// card view of travel plan, used on home page etc.
+// "condensed" view of travel plan, used on home page etc.
 export default function PlanView(idx : number) {
     const { loading, error, data } = useQuery(GET_PLANS);
 
@@ -38,69 +38,58 @@ export default function PlanView(idx : number) {
     let plan_budget_display = "" as String;
     for (let i = 0; i < plan_budget; i++) {
         plan_budget_display = plan_budget_display.concat("$".toString());
-    }    
-    
-    let plan_tags_display = []
-    for (let i = 0; i < 10; i++) {
-        if (plan_tags[i] != undefined)
-        {
-            plan_tags_display.push(
-                <Box key={i} bg="info.100" rounded="lg" justifyContent="center">
-                    <Text ml={"1"} mr={"1"}>
-                            {plan_tags[i]}
-                    </Text>
-                </Box>
-            );
-        }
-    }
+    }      
 
     return (
         <NativeBaseProvider>
-            <Box bg="white" rounded="lg" overflow="hidden" borderWidth="0" width={"80%"} shadow={"2"}>
-                <VStack width={'100%'} ml={"1.5"} mr={"1"}>
-                    
-                    <HStack width={'100%'} mb={"1"} mt={"1"} mr={"1"} justifyContent="space-between">
-                        <Text bold alignSelf={'center'} fontSize={"lg"}>
+            <Box bg="tertiary.300" borderWidth="1" width={"80%"} >
+                <VStack width={'100%'}>
+                    <HStack alignItems={'center'} width={'100%'}>
+                        <Box width={'70%'}>
+                        <Image source={{
+                            uri: "https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/heroImage1/PowerfulReasons_hero.jpg"
+                            }} alt="Alternate Text" width={'100%'} height={"120"} />
+                        
+                        </Box>
+                        
+                        <VStack space={1} alignItems={"center"} width={'30%'}>
+                            <Box bg="secondary.200">
+                                <Text>
+                                    {plan_tags[0]}
+                                </Text>
+                            </Box>
+                            <Box bg="secondary.200">
+                                <Text>
+                                    {plan_tags[1]}
+                                </Text>
+                            </Box>
+                            <Box bg="secondary.200">
+                                <Text>
+                                    {plan_tags[2]}
+                                </Text>
+                            </Box>
+                        </VStack>
+                    </HStack>
+                    <HStack width={'80%'}>
+                        <Text bold width={'100%'} alignSelf={'center'} fontSize={"lg"}>
                             {plan_name}
-                        </Text>      
-                        <Icon as={Entypo} name={"dots-three-horizontal"} size={"lg"} color={"black"} mr={"3"} alignSelf={'baseline'}/> 
+                        </Text>             
                     </HStack>
                     
-                    <HStack width={"95%"} mb={"1"} mt={"1"} justifyContent="space-between">
-                        <HStack justify-content={"start"} space={2}>
-                            <Box bg="yellow.100" rounded="lg" justifyContent="center">
-                                <Text alignSelf={'center'} ml={"1"} mr={"1"}>
-                                    {plan_rating}/5
-                                    {/* <SunIcon color={"black"} alignSelf={'baseline'} ml={"1"} mr={"1"}/>  */}
-                                </Text>
-                            </Box>
-                            <Box bg="red.100" rounded="lg" justifyContent="center">
-                                <Text  alignSelf={'center'} ml={"1"} mr={"1"}>
-                                    {plan_budget_display}
-                                </Text>
-                            </Box>
-                        </HStack>
-                        
-                        <Text justifyContent={"center"}>
-                            Last Updated: Jun 6, 2022
+                    <HStack width={'80%'}>
+                        <Text width={'100%'} alignSelf={'center'}>
+                            {plan_rating}/5
+                        </Text>
+                        <Text width={'100%'} alignSelf={'center'}>
+                            {plan_budget_display}
                         </Text>                     
                     </HStack>
 
-                    
-                    <Center width={'100%'} justifyContent={'center'} ml={"-1.5"} mb={"1"} mt={"1"}>
-                        <Image rounded="lg" source={{
-                            uri: "https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/heroImage1/PowerfulReasons_hero.jpg"
-                            }} alt="Alternate Text" height={"120"} width={"325"} resizeMode={"contain"} alignSelf={"center"}/>
-                    </Center>
-
-                    <Box width={'90%'} mb={"1"} mt={"1"}>
-                        <Text numberOfLines={2} ellipsizeMode='tail' width={'100%'} alignSelf={'center'} ml={"1.5"} mr={"1.5"}>
+                    <Box width={'80%'}>
+                        <Text width={'100%'} alignSelf={'center'}>
                             {plan_description}
                         </Text>             
                     </Box>
-                    <HStack space={1} alignItems={"center"} width={'30%'} mb={"2"} mt={"1"}>
-                        {plan_tags_display}
-                    </HStack>
                 </VStack>
             </Box>    
         </NativeBaseProvider>
