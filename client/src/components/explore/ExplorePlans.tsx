93c5e7adc8057f7a80ee98d5e20fe12c0f3f42c8
h255kim <h255kim@uwaterloo.ca>
2022-07-03 04:56:40 -0400

    Sort travel plans

--- client/src/components/explore/ExplorePlans.tsx
+++ client/src/components/explore/ExplorePlans.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-import { Button, Box, ScrollView, HStack, Stack } from "native-base";
+import {
+  Button,
+  Box,
+  ScrollView,
+  HStack,
+  Heading,
+  Center,
+  Input,
+  VStack,
+  Actionsheet,
+  useDisclose,
+  Icon,
+  IconButton,
+  Flex,
+} from "native-base";
 import { gql, useQuery, NetworkStatus } from "@apollo/client";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import PlanCardSmall from "../PlanCardSmall";
+import Filters from "../Filters";
 
 export const GET_FILTERED_PLANS = gql`
   query getFilteredPlans($input: FilterInput!) {
@@ -30,16 +47,49 @@ export default function ExplorePlans() {
       notifyOnNetworkStatusChange: true,
     }
   );
-  const [popularPlans, setPopularPlans] = React.useState([]);
-  const [nationalPlans, setNationalPlans] = React.useState([]);
-  const [internationalPlans, setInternationalPlans] = React.useState([]);
-  const [budgetPlans, setBudgetPlans] = React.useState([]);
+
+  const { isOpen, onOpen, onClose } = useDisclose();
+  //   const [allPlans, setAllPlans] = React.useState(null);
+  //   const [popularPlans, setPopularPlans] = React.useState([]);
+  //   const [nationalPlans, setNationalPlans] = React.useState([]);
+  //   const [internationalPlans, setInternationalPlans] = React.useState([]);
+  //   const [budgetPlans, setBudgetPlans] = React.useState([]);
+  //   const [luxuryPlans, setLuxuryPlans] = React.useState([]);
+
+  //   React.useEffect(() => {
+  //     const popular = data.filteredPlans.filter((plan) => plan.rating >= 4);
+  //     const national = data.filteredPlans.filter((plan) =>
+  //       plan.countries.includes(CURRENT_LOCATION)
+  //     );
+  //     const international = data.filteredPlans.filter(
+  //       (plan) => !plan.countries.includes(CURRENT_LOCATION)
+  //     );
+  //     const budget = data.filteredPlans.filter((plan) => plan.budget === 1);
+  //     const luxury = data.filteredPlans.filter((plan) => plan.budget >= 4);
+
+  //     setPopularPlans(popular);
+  //     setNationalPlans(national);
+  //     setInternationalPlans(international);
+  //     setBudgetPlans(budget);
+  //     setLuxuryPlans(luxury);
+  //   }, []);
+
   if (networkStatus === NetworkStatus.refetch) return "Refetching!";
   if (loading) return "Loading...";
   if (error) return `Error! ${error}`;
 
-  const listPlans = () => {
-    return data.filteredPlans.map((plan) => {
+  const popularPlans = data.filteredPlans.filter((plan) => plan.rating >= 4);
+  const nationalPlans = data.filteredPlans.filter((plan) =>
+    plan.countries.includes(CURRENT_LOCATION)
+  );
+  const internationalPlans = data.filteredPlans.filter(
+    (plan) => !plan.countries.includes(CURRENT_LOCATION)
+  );
+  const budgetPlans = data.filteredPlans.filter((plan) => plan.budget === 1);
+  const luxuryPlans = data.filteredPlans.filter((plan) => plan.budget >= 4);
+
+  const displayPlans = (plansList) => {
+    return plansList.map((plan) => {
       return (
         <Box key={plan.id} mr="1">
           {PlanCardSmall(plan)}
@@ -62,7 +112,7 @@ export default function ExplorePlans() {
   };
 
   return (
-    <Box>
+    <Box mb="200">
       <Button
         onPress={() => {
           console.log("refetching...");
@@ -75,9 +125,111 @@ export default function ExplorePlans() {
       >
         Refetch
       </Button>
-      <ScrollView horizontal={true}>
-        <HStack>{listPlans()}</HStack>
-      </ScrollView>
+      <Box>
+        <Center>
+          <Actionsheet isOpen={isOpen} onClose={onClose}>
+            <Actionsheet.Content>
+              <Filters />
+            </Actionsheet.Content>
+          </Actionsheet>
+        </Center>
+        {/* <SafeAreaView> */}
+        <VStack w="90%" alignSelf="center">
+          <Flex direction="row" mb="2">
+            <Input
+              InputLeftElement={
+                <Icon
+                  as={
+                    <Ionicons
+                      name="search-outline"
+                      color="gray"
+                      style={{ marginLeft: 12 }}
+                    />
+                  }
+                />
+              }
+              flexGrow={1}
+              size="lg"
+              placeholder="Search by name or #tag"
+            />
+            <VStack alignSelf="center" ml="1">
+              <IconButton
+                variant="ghost"
+                onPress={onOpen}
+                icon={
+                  <MaterialCommunityIcons
+                    name="filter-outline"
+                    size={24}
+                    color="gray"
+                  />
+                }
+              />
+            </VStack>
+          </Flex>
+
+          <ScrollView _contentContainerStyle={{ paddingBottom: 160 }}>
+            {popularPlans.length ? (
+              <Box>
+                <Heading size="sm" mt="6">
+                  Most Popular
+                </Heading>
+                <ScrollView horizontal={true} my="2">
+                  <HStack>{displayPlans(popularPlans)}</HStack>
+                </ScrollView>
+              </Box>
+            ) : null}
+
+            {nationalPlans.length ? (
+              <Box>
+                <Heading size="sm" mt="6">
+                  National Destinations
+                </Heading>
+
+                <ScrollView horizontal={true} my="2">
+                  <HStack>{displayPlans(nationalPlans)}</HStack>
+                </ScrollView>
+              </Box>
+            ) : null}
+
+            {internationalPlans.length ? (
+              <Box>
+                <Heading size="sm" mt="6">
+                  International Destinations
+                </Heading>
+
+                <ScrollView horizontal={true} my="2">
+                  <HStack>{displayPlans(internationalPlans)}</HStack>
+                </ScrollView>
+              </Box>
+            ) : null}
+
+            {budgetPlans.length ? (
+              <Box>
+                <Heading size="sm" mt="6">
+                  Budget Travel
+                </Heading>
+
+                <ScrollView horizontal={true} my="2">
+                  <HStack>{displayPlans(budgetPlans)}</HStack>
+                </ScrollView>
+              </Box>
+            ) : null}
+
+            {luxuryPlans.length ? (
+              <Box>
+                <Heading size="sm" mt="6">
+                  Luxury Travel
+                </Heading>
+
+                <ScrollView horizontal={true} my="2">
+                  <HStack>{displayPlans(luxuryPlans)}</HStack>
+                </ScrollView>
+              </Box>
+            ) : null}
+          </ScrollView>
+        </VStack>
+        {/* </SafeAreaView> */}
+      </Box>
     </Box>
   );
 }
