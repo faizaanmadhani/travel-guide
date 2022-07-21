778c5719cac159f43078707475c95ace6136ebc8
Jiawen Zhu <s234zhu@uwaterloo.ca>
2022-07-21 00:45:10 -0400

    Update Explore Page to work with Wishlist

--- client/src/views/WishlistView.tsx
+++ client/src/views/WishlistView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import { Button, Box, ScrollView, Center, Text} from "native-base";
 import { useWindowDimensions } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 // For testing purposes
 // import TravelPlanPage from "./PlanMain";
@@ -24,7 +25,7 @@ const sampleData : PlanView_Data =
     countries: ["Canada"],
     months: ["January"]};
 
-const GET_WISHLIST = gql`
+export const GET_WISHLIST = gql`
   query getWishlistPlans ($id : String!) {
     getWishlistPlans (id: $id) {
       id
@@ -39,28 +40,6 @@ const GET_WISHLIST = gql`
   }
 `;
 
-export const ADD_WISHLIST = gql`
-mutation addWishlist($input : AddWishlistPlanInput!) {
-    addWishlistPlan (input : $input)
-    {
-      id
-      name
-      wishlistPlans
-    }
-  }  
-`;
-
-export const REMOVE_WISHLIST = gql`
-mutation removeWishlist($input : AddWishlistPlanInput!) {
-    removeWishlistPlan (input : $input)
-    {
-      id
-      name
-      wishlistPlans
-    }
-  }  
-`;
-
 export const UPDATE_WISHLIST = gql`
 mutation updateWishlist($input : AddWishlistPlanInput!) {
     updateWishlistPlan (input : $input)
@@ -73,17 +52,15 @@ mutation updateWishlist($input : AddWishlistPlanInput!) {
 `;
 
 export default function WishlistView(userID : String) {
+    useFocusEffect(
+        React.useCallback(() => {refetch()}, [])
+    );
+
     const [updateWishlist, { data : data1, loading : loading1, error : error1 }] = useMutation(UPDATE_WISHLIST,
         {refetchQueries: [{query: GET_WISHLIST}], onCompleted: () => refetch()});
 
     if (loading1) console.log("Loading...");;
     if (error1) console.log(`Error! ${error1.message}`);
-
-    // const [removeWishlist, { data:data2, loading:loading2, error:error2 }] = useMutation(REMOVE_WISHLIST,
-    //     {refetchQueries: [], ignoreResults: true});
-
-    // if (loading2) console.log("Loading...");;
-    // if (error2) console.log(`Error! ${error2.message}`);
 
     const { data, error, loading, refetch } =  useQuery(GET_WISHLIST, {
       variables : {id : userID},
@@ -96,7 +73,7 @@ export default function WishlistView(userID : String) {
   if (loading)
     {
         console.log(`Loading`);
-        return <Text>"Loading ..."</Text> ;
+        return <Text>Loading ...</Text> ;
     }
 
     if (error)
@@ -105,23 +82,13 @@ export default function WishlistView(userID : String) {
         return <Text>{`Error! ${error.message}`}</Text>;
     };
 
-//   function displayPlans(plansList) {
-//     console.log("DISPLAY", plansList)
-//     if (plansList != undefined)
-//     {
-//       return plansList.map((plan) => {
-//         return <Box key={plan.id}>{PlanCardWishlist(plan)}</Box>;
-//       });
-//     }
-
-//   };
     const plans = data.getWishlistPlans;
     let planViews = []
     for (let i = 0; plans[i] != undefined; i++)
     {
         planViews.push(<Box key={i}>{PlanCardWishlist(plans[i], updateWishlist, userID)}</Box>)
     }
-    planViews.push(<Box key={100}>{PlanCardWishlist(sampleData, updateWishlist, userID)}</Box>)
+    // planViews.push(<Box key={100}>{PlanCardWishlist(sampleData, updateWishlist, userID)}</Box>)
 
   return(
         <Box> {planViews} </Box>
